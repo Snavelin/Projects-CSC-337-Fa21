61291203b4ea81efe5353d2dd92ea9f8a3bc5332
Snavelin <49351496+Snavelin@users.noreply.github.com>
2021-09-20 17:03:25 -0700

    Worte comments for each script file

--- assg5/public_html/caesar.js
+++ assg5/public_html/caesar.js
@@ -1,19 +1,33 @@
+/*
+* Author: Dylan Snavely
+* Purpose: Defines code related to the execution of the Caesar Cipher encryption method.
+*/
+
+/*
+* Fetches the text from the text input field in the document.
+*/
 function getText() {
     let txtIn = document.getElementById("textInput");
     return txtIn.value;
 }
 
+/*
+* Fetches the shift amount from the range input field in the document.
+*/
 function getCipherAmount() {
     let cipherAmt = document.getElementById("cipherAmount");
     return Number.parseInt(cipherAmt.value);
 }
 
+/*
+* Performs the Caesar Cipher on the contents of the html text input.
+*/
 function caesarCipher() {
     let originalString = getText();
     let cipherAmt = getCipherAmount();
     let encryptedString = "";
-    for (let i = 0; i < originalString.length; i++) {
-        let char = originalString[i].toUpperCase();
+    for (let char of originalString) {
+        char = char.toUpperCase();
         // Check if this char is a letter.
         if (char.toUpperCase() !== char.toLowerCase()) {
             // Char is a letter. Encrypt it.
@@ -24,6 +38,7 @@ function caesarCipher() {
             encryptedString += char;
         }
     }
+    // Display the encrypted text in the top portion of the page
     let outputSection = document.getElementById("caesarCipherSection");
     outputSection.innerText = encryptedString;
 }
--- assg5/public_html/square.js
+++ assg5/public_html/square.js
@@ -1,13 +1,26 @@
-function shuffle(array) {
-    for (let i = 0; i < array.length; i++) {
+/*
+* Author: Dylan Snavely
+* Purpose: Defines code related to the execution of the Square Cipher encryption method.
+*/
+
+/*
+* Shuffles the contents of the given list.
+* Params:
+*       list -- the list to be shuffled.
+*/
+function shuffle(list) {
+    for (let i = 0; i < list.length; i++) {
         let j = Math.floor(Math.random() * (i + 1));
-        let swap = array[i];
-        array[i] = array[j];
-        array[j] = swap;
+        let swap = list[i];
+        list[i] = list[j];
+        list[j] = swap;
     }
-    return array;
+    return list;
 }
 
+/*
+* Shuffles the contents of the square for the Square Cipher.
+*/
 function updateSquare() {
     let squareLetters = document.getElementsByTagName("td");
     let chars = [];     // Holds the chars of the square in order to shuffle them
@@ -21,19 +34,28 @@ function updateSquare() {
     }
 }
 
+/*
+* Performs the Square Cipher on the contents of the html text input.
+* This does not update the square. For that, see updateSquare().
+*/
 function squareCipher() {
     let originalText = getText();
     let squareLetters = document.getElementsByTagName("td");
     let encryptedText = "";
+    // Encrypt each char
     for (let char of originalText) {
         char = char.toUpperCase();
+        // Check if this char is a letter.
         if (char.toUpperCase() !== char.toLowerCase() && char !== 'Z') {
-            let charCode = (char.charCodeAt(0) - 65);
-            encryptedText += squareLetters[charCode].innerText;
+            // Is a letter. Encrypt it.
+            let charPos = (char.charCodeAt(0) - 65);  // Get letter's position in alphabet, starting at 0.
+            encryptedText += squareLetters[charPos].innerText;
         } else {
+            // Otherwise, add it to encrypted string unchanged.
             encryptedText += char;
         }
     }
+    // Display the encrypted text in the bottom portion of the page
     let outputSection = document.getElementById("squareCipherSection");
     outputSection.innerText = encryptedText;
 }
