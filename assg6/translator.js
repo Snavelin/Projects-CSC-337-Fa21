ad32fbf32f6d6a6c8dd029f91778de4035654881
Snavelin <49351496+Snavelin@users.noreply.github.com>
2021-09-28 18:21:59 -0700

    Wrote comments

--- assg6/translator.js
+++ assg6/translator.js
@@ -1,30 +1,41 @@
+/*
+* Author: Dylan Snavely
+* Purpose: A basic human language translation service. This program creates a database of
+* translations, then opens a server hosted at localhost to receive requests. A request is made
+* through the URL with the search terms at the end.
+* Example:
+*       localhost:5000/translate/e2g/car+airplane+climb+chair
+*/
+
 const fs = require('fs');
 const readline = require('readline');
 const http = require('http');
 
+// Create the database, then open a server and process requests
 createDatabase().then(function (dictionaries) {
     const port = 5000;
     const hostname = 'localhost';
     const server = http.createServer(
         function (req, res) {
+            // This function processes each translation request
             res.statusCode = 200;
 
             let urlComponents = req.url.split('/').slice(1);
             console.log(`Requesting ${req.url}`);
             console.log(urlComponents);
-            // todo: Check if request is to translate, otherwise send 'OK'
             let resContent = '';
+            // Check if request is to translate, otherwise send 'OK'
             if (urlComponents.length < 2 || urlComponents[0] !== 'translate' || !(urlComponents[1] in dictionaries)) {
                 resContent = 'OK';
             } else {
+                // Translate words from URL
                 let mode = urlComponents[1];
                 let words = urlComponents[2].split('+');
                 for (const word of words) {
                     resContent += `${dictionaries[mode][word]} `;
                 }
             }
-            // todo: Translate words from url
-
+            // Send translation to the browser
             res.end(resContent);
         }
     );
@@ -37,7 +48,10 @@ createDatabase().then(function (dictionaries) {
 });
 
 
-
+/*
+* Creates a database of dictionaries for translation. Each dictionary is mapped to its respective
+* 'mode' (i.e. specifying which languages to translate between).
+*/
 async function createDatabase() {
     const e2sDict = await createDictionaryFromFile('Spanish.txt');
     const e2gDict = await createDictionaryFromFile('German.txt');
@@ -54,6 +68,12 @@ async function createDatabase() {
     };
 }
 
+/*
+* Creates a dictionary of fromWords to toWords where fromWords are in one language an toWords are
+* in a different language.
+* Params:
+*       filename -- the path to a file containing the translation table
+*/
 async function createDictionaryFromFile(filename) {
     const file = readline.createInterface({
         input: fs.createReadStream(filename),
@@ -88,6 +108,12 @@ function reverseDictionary(dict) {
     return revDict;
 }
 
+/*
+* Creates a dictionary with the keys of dict1 mapped to the values of dict2.
+* Params:
+*       dict1 -- the dictionary that will share keys with a new dictionary
+*       dict2 -- the dictionary that will share values with a new dictionary
+*/
 function map(dict1, dict2) {
     let result = {};
     for (const dict1Key in dict1) {
