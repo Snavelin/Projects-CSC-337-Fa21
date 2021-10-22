08c580fedfc0799bf854d91cd45ecaa404c2b305
Snavelin <49351496+Snavelin@users.noreply.github.com>
2021-10-21 17:13:59 -0700

    Server now adds messages to the database. Server now returns all messages in database when pinged.

--- assg8/chatty/server.js
+++ assg8/chatty/server.js
@@ -7,14 +7,42 @@ app.use(express.static('public_html'));
 const port = 5000;
 const hostname = 'localhost';
 
+const db = mongoose.connection;
+const mongoDBURL = `mongodb://${hostname}/chats`;
+mongoose.connect(mongoDBURL, { useNewUrlParser: true });
+
+db.on('error', function () {
+   console.error('Error on connecting to mongoDB');
+});
+
+// Schemas
+
+const Schema = mongoose.Schema;
+const ChatMessageSchema = new Schema({
+    time: Number,
+    alias: String,
+    message: String
+});
+const ChatMessage = mongoose.model('ChatMessage', ChatMessageSchema);
+
 // Express routes
 
 app.post('/chats/post/:alias/:message', function (req, res) {
     console.log('received a message');
+    const alias = req.params.alias
+    const message = req.params.message
+    const thisMessage = new ChatMessage({ time: Date.now(), alias: alias, message: message });
+    thisMessage.save();
 });
 
 app.get('/chats', function (req, res) {
-
+    const query = ChatMessage.find({});
+    query.select('alias message');
+    query.sort({ time: 1 });
+    query.exec(function (error, result) {
+        if (error) console.error('Error getting chat messages');
+        res.end(JSON.stringify(result));
+    });
 });
 
 app.listen(port, hostname, function () {
