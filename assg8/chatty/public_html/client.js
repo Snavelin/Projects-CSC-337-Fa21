da42a58fc3ce88bcc380acaece648746f63875e8
Snavelin <49351496+Snavelin@users.noreply.github.com>
2021-10-21 17:15:00 -0700

    Client now pings server every second to fetch all messages on server

--- assg8/chatty/public_html/client.js
+++ assg8/chatty/public_html/client.js
@@ -1,4 +1,5 @@
 const URL_BASE = 'http://localhost:5000';
+setInterval(fetchMessages, 1000);  // ping the server for messages every second
 
 function sendMessage() {
     const alias = document.getElementById('alias').value;
@@ -18,5 +19,24 @@ function sendMessage() {
 }
 
 function fetchMessages() {
+    const request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === request.DONE && request.status === 200) {
+            const responseData = JSON.parse(request.responseText);
+            console.log(responseData);
+            let chatHtml = '';
+            for (const record of responseData) {
+                let alias = record.alias;
+                let message = record.message;
+                chatHtml += `<div class="chatMessage"><b>${alias}: </b>${message}</div>`;
+            }
+            const chatSection = document.getElementById('chatSection');
+            chatSection.innerHTML = chatHtml;
+        }
+    }
 
+    const url = `${URL_BASE}/chats`;
+    console.log(`attempting GET ${url}`);
+    request.open('GET', url);
+    request.send();
 }
